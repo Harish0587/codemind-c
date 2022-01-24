6ec399f0d06538b534825d3e3763974b5a18d527
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-01-24 11:35:57 +0530

    #include<stdio.h> int main() { 	int num,rem,sum=0,pro=1; 	scanf("%d",&num); 	for (;num;) 	{ 		rem=num%10; 		sum=sum+rem; 		pro=pro*rem; 		num=num/10; 	} 	if (pro==sum) 	{ 		printf("Spy Number"); 	} 	else 	{ 		printf("Not Spy Number"); 	} }

---
+++ Spy_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int num,rem,sum=0,pro=1;
+	scanf("%d",&num);
+	for (;num;)
+	{
+		rem=num%10;
+		sum=sum+rem;
+		pro=pro*rem;
+		num=num/10;
+	}
+	if (pro==sum)
+	{
+		printf("Spy Number");
+	}
+	else
+	{
+		printf("Not Spy Number");
+	}
+}
