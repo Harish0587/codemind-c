8d4c071958507662409d93dffeb0dde97391da3e
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-02-28 11:34:10 +0530

    #include<stdio.h> int main() {  int a,b,c,next,num;  scanf("%d", &num);  if((num==0)||(num==1))  {      printf("%d",num);  }
     else
     {
       a=0;
       b=1;
       c=a+b;
       while(c<num)
       {
         a=b;
         b=c;
         c=a+b;
       }
       if(c==num)
       {
         printf("True",num);
       }
       
       else
       {
         printf("False",num);
     }
    }
    }

---
+++ Fibonacci_or_not.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+ int a,b,c,next,num;
+ scanf("%d", &num);
+ if((num==0)||(num==1))
+ {
+     printf("%d",num);
+ }
+  
+ else
+ {
+   a=0;
+   b=1;
+   c=a+b;
+   while(c<num)
+   {
+     a=b;
+     b=c;
+     c=a+b;
+   }
+   if(c==num)
+   {
+     printf("True",num);
+   }
+   
+   else
+   {
+     printf("False",num);
+ }
+}
+}
