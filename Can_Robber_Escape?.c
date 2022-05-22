8516c0072ca548414ac53efd5120b50ae0a9de60
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-05-22 10:59:32 +0530

    #include<stdio.h> int main() {     int n,x[50],odd=0,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&x[i]);     }     for(i=0;i<n;i++)     {         if(x[i]%2!=0)         {             odd++;         }     }     if(odd<=2)     {         printf("YES");     }     else     {         printf("NO");     } }

---
+++ Can_Robber_Escape?.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,x[50],odd=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&x[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(x[i]%2!=0)
+        {
+            odd++;
+        }
+    }
+    if(odd<=2)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
+}
