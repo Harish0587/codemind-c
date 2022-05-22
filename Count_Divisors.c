c2f0a2cf0a929ac16d655c1cf2e8c8eebc27f127
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-05-22 11:08:48 +0530

    #include<stdio.h> int main() {     int a,b,c,i,count=0;     scanf("%d%d%d",&a,&b,&c);     for(i=a;i<=b;i++)     {         if(i%c==0)         count=count+1;     }     printf("%d",count); }

--- Count_Divisors.c
+++ Count_Divisors.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,j,r,k,count=0;
-    scanf("%d%d%d",&j,&r,&k);
-    for(i=j;i<=r;i++)
+    int a,b,c,i,count=0;
+    scanf("%d%d%d",&a,&b,&c);
+    for(i=a;i<=b;i++)
     {
-        if(i%k==0)
-        {
-            count=count+1;
-        }
+        if(i%c==0)
+        count=count+1;
     }
     printf("%d",count);
 }
+
