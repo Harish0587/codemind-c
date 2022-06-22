0d3088ac0438896410f43d12d7ac97685fd383b1
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-06-22 22:44:13 +0530

    #include<stdio.h> int main() {     int l,n,w,h,i;     scanf("%d",&l);    // printf("%d ",l);     scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d%d",&w,&h);
            //printf("%d %d
    ",w,h);
           if(w<l || h<l)
             {
                printf("UPLOAD ANOTHER
    ");
             }
            else if(w>=l && h>=l)
            {
            if(w==h)
            {
                printf("ACCEPTED
    ");
            }
            else
            {
                printf("CROP IT
    ");
            }
            }
        }
    }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int l,n,w,h,i;
+    scanf("%d",&l);
+   // printf("%d
+",l);
+    scanf("%d",&n);
+    
+    for(i=0;i<n;i++)
+    {
+        scanf("%d%d",&w,&h);
+        //printf("%d %d
+",w,h);
+       if(w<l || h<l)
+         {
+            printf("UPLOAD ANOTHER
+");
+         }
+        else if(w>=l && h>=l)
+        {
+        if(w==h)
+        {
+            printf("ACCEPTED
+");
+        }
+        else
+        {
+            printf("CROP IT
+");
+        }
+        }
+    }
+}
+
