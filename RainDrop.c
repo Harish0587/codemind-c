5e123b5c37cc51447193d8cdd72c97b18dc9d6ce
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-06-22 21:35:10 +0530

    #include<stdio.h> int main() {     int n;     char i[100];     scanf("%d",&n);     if(n%3==0 && n%5!=0 && n%7!=0)     {         printf("Pling");     }     else if(n%5==0 && n%3!=0 && n%7!=0)     {         printf("Plang");     }     else if(n%7==0 && n%5!=0 && n%3!=0)     {         printf("Plong");     }     else if(n%3==0 && n%5==0)     {         printf("PlingPlang");     }     else if(n%3==0 && n%7==0)     {         printf("PlingPlong");     }     else if(n%5==0 && n%7==0)     {         printf("PlangPlong");     }     else if(n%3==0 && n%5==0 && n%7==0)     {         printf("PlingPlangPlong");     }     else     {         printf("%d",n);     } }

---
+++ RainDrop.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    char i[100];
+    scanf("%d",&n);
+    if(n%3==0 && n%5!=0 && n%7!=0)
+    {
+        printf("Pling");
+    }
+    else if(n%5==0 && n%3!=0 && n%7!=0)
+    {
+        printf("Plang");
+    }
+    else if(n%7==0 && n%5!=0 && n%3!=0)
+    {
+        printf("Plong");
+    }
+    else if(n%3==0 && n%5==0)
+    {
+        printf("PlingPlang");
+    }
+    else if(n%3==0 && n%7==0)
+    {
+        printf("PlingPlong");
+    }
+    else if(n%5==0 && n%7==0)
+    {
+        printf("PlangPlong");
+    }
+    else if(n%3==0 && n%5==0 && n%7==0)
+    {
+        printf("PlingPlangPlong");
+    }
+    else
+    {
+        printf("%d",n);
+    }
+}
