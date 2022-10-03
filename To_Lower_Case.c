ed58560f03a6f7c6ca75c6f42adf02d4185897b4
Harish divadari <96948622+Harish0587@users.noreply.github.com>
2022-10-03 11:38:12 +0530

    #include<stdio.h> #include<string.h> int main() {     char str[100];     scanf("%s",str);     int i;     for(i=0;str[i]!=NULL;i++)     {         if(str[i]>=65&&str[i]<=90)         {             str[i]=str[i]+32;         }     }     printf("%s",str); }

---
+++ To_Lower_Case.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100];
+    scanf("%s",str);
+    int i;
+    for(i=0;str[i]!=NULL;i++)
+    {
+        if(str[i]>=65&&str[i]<=90)
+        {
+            str[i]=str[i]+32;
+        }
+    }
+    printf("%s",str);
+}
